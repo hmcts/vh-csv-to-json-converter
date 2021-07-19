ea26f470aad3db67a67712c01fa4e15894966489
alberto-hortiguela <alberto.hortiguela@hmcts.net>
2021-07-19 10:05:38 +0100

    Add project files.

---
+++ TranslateJsonFlatten/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TranslateJsonFlatten
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var fi = new FileInfo(args.Length > 0 ? args[0] : throw new ArgumentException("Please provide file"));
+            if (!fi.Exists)
+            {
+                throw new ArgumentException("Unable to locate file");
+            }
+
+            switch (fi.Extension.ToLower())
+            {
+                case ".json":
+                    var jsonText = File.ReadAllText(fi.FullName);
+                    var csvLines = new TranslationFileConversionService().ConvertToCsv(jsonText, "Key,English,Welsh");
+                    var csvOutputFile = GetOutputFile(args[0], ".csv");
+                    File.WriteAllLines(csvOutputFile.FullName, csvLines);
+                    break;
+                case ".csv":
+                    var lines = File.ReadAllLines(fi.FullName);
+                    var jsonString = new TranslationFileConversionService().ConvertToJson(lines);
+                    var jsonOutputFile = GetOutputFile(args[0], ".json");
+                    File.WriteAllText(jsonOutputFile.FullName, jsonString);
+                    break;
+                default:
+                    throw new ArgumentException($"Invalid file type {fi.Extension}");
+            }
+            
+
+        }
+
+        static FileInfo GetOutputFile(string inputPath, string outputExtension)
+        {
+            var inputFi = new FileInfo(inputPath);
+            var outputFile = new FileInfo(inputFi.Directory.FullName + "\\" + inputFi.Name.Replace(inputFi.Extension, outputExtension));
+            outputFile.Delete();
+            return outputFile;
+        }
+    }
+}
---
+++ TranslateJsonFlatten/TranslationFileConversionService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TranslateJsonFlatten
+{
+    public class TranslationFileConversionService
+    {
+        public IEnumerable<string> ConvertToCsv(string json, string headers = null)
+        {
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonStream(ms)
+                    .Build();
+
+                var outputCsv = new List<string>();
+                if (headers != null)
+                {
+                    outputCsv.Add(headers);
+                }
+
+                outputCsv.AddRange(config.AsEnumerable().Where(kvp => !string.IsNullOrEmpty(kvp.Value)).Select(kvp => $"{kvp.Key},\"{kvp.Value}\""));
+                return outputCsv;
+            }
+        }
+
+        public string ConvertToJson(string[] csvRows)
+        {
+            var rowsList = csvRows.ToList();
+            rowsList.RemoveAt(0);
+            dynamic dynamicObject = new ExpandoObject();
+            foreach (var line in rowsList)
+            {
+                IDictionary<string, object> dynamicDictionary = dynamicObject;
+                var key = line.Split(',')[0];
+                var segments = key.Split(':');
+                for (var i = 0; i < segments.Length - 1; i++)
+                {
+                    if (!dynamicDictionary.ContainsKey(segments[i]))
+                    {
+                        dynamicDictionary.Add(segments[i], new ExpandoObject());
+                    }
+                    dynamicDictionary = dynamicDictionary[segments[i]] as ExpandoObject;
+                }
+
+                //var value = line.Replace($"{key},", string.Empty);                
+                //value = value.Trim(',');
+                //var value = line.Split(',')[1];
+
+                int index = line.IndexOf(',', line.IndexOf(','));
+                var value = line.Substring(index + 1).Replace("\"", string.Empty);
+
+                dynamicDictionary.Add(segments[segments.Length - 1], value);
+            }
+
+            return JsonConvert.SerializeObject(dynamicObject, Formatting.Indented);
+        }
+    }
+}
